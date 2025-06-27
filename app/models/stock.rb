6dced2f3e2632bbcae4a0f06d2f557e6f6c916bf
Mariano Ranucci <ranucci.mariano@gmail.com>
2025-06-27 13:42:55 -0300

    Refactored navbar and my_portfolio view

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -19,12 +19,15 @@ class Stock < ApplicationRecord
   def self.new_lookup(ticker_symbol)
     url = get_api_url('GLOBAL_QUOTE', ticker_symbol)
     response = HTTParty.get(url)
-    if response.code == 200 && response.parsed_response['Global Quote']['05. price']
+    if response.code == 200 &&
+      response.parsed_response['Global Quote'].present? &&
+      response.parsed_response['Global Quote']['05. price']
       last_price = response.parsed_response['Global Quote']['05. price']
       name = company_lookup(ticker_symbol)
       new(ticker: ticker_symbol, name: name, last_price: last_price)
     else
       nil
+      # new(ticker: ticker_symbol, name: 'mock-name', last_price: 10)
     end
   end
 
