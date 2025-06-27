8a68d14f69dc513fff518c8a2617da9db27d2627
Mariano Ranucci <ranucci.mariano@gmail.com>
2025-06-27 14:48:57 -0300

    Added create user_stock action with restrictions

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def under_stock_limit?
+    stocks.count < 10
+  end
+
+  def stock_already_tracked?(ticker_symbol)
+    stocks.find_by(ticker: ticker_symbol).present?
+  end
+
+  def can_track_stock?(ticker_symbol)
+    under_stock_limit? && !stock_already_tracked?(ticker_symbol)
+  end
 end
