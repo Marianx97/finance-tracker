97a5d1db630d002c60810a548d022c37342c2be4
Mariano Ranucci <ranucci.mariano@gmail.com>
2025-06-27 09:48:31 -0300

    Updated stock controller and my_portfolio view to use turbo

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -2,15 +2,16 @@ class StocksController < ApplicationController
   def search
     if params[:stock].present?
       @stock = Stock.new_lookup(params[:stock])
-      if @stock
-        render 'users/my_portfolio'
-      else
-        flash[:alert] = 'Please enter a valid symbol to search'
-        redirect_to my_portfolio_path
+
+      puts '@stock' + @stock.to_s
+
+      if !@stock
+        flash.now[:alert] = 'Please enter a valid symbol to search'
       end
     else
-      flash[:alert] = 'Please enter a symbol to search'
-      redirect_to my_portfolio_path
+      flash.now[:alert] = 'Please enter a symbol to search'
     end
+
+    render turbo_stream: turbo_stream.replace('results_turbo_stream', partial: 'users/results')
   end
 end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -14,7 +14,7 @@ class Stock < ApplicationRecord
   def self.new_lookup(ticker_symbol)
     url = get_api_url('GLOBAL_QUOTE', ticker_symbol)
     response = HTTParty.get(url)
-    if response.code == 200 && response.parsed_response['Global Quote']
+    if response.code == 200 && response.parsed_response['Global Quote']['05. price']
       last_price = response.parsed_response['Global Quote']['05. price']
       name = company_lookup(ticker_symbol)
       new(ticker: ticker_symbol, name: name, last_price: last_price)
