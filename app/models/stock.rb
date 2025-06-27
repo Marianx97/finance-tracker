ee076b175e4d63ec1c0e0254a6d22546f7758467
Mariano Ranucci <ranucci.mariano@gmail.com>
2025-06-27 11:04:57 -0300

    Added UserStocks relation

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,6 +1,11 @@
 require 'httparty'
 
 class Stock < ApplicationRecord
+  has_many :user_stocks
+  has_many :users, through: :user_stocks
+
+  validates :name, :ticker, presence: true
+
   def self.company_lookup(ticker_symbol)
     url = get_api_url('OVERVIEW', ticker_symbol)
     response = HTTParty.get(url)
